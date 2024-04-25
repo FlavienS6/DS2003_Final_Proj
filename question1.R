d54f3eaf2428f28fa86e63351ba3231fde2cebcf
Alexander Daivs <wfy8cn@virginia.edu>
2024-04-25 15:38:53 -0400

    changed q1

--- question1.R
+++ question1.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(readr)
+library(ggplot2)
 
 card<-read_delim("cardio_train.csv",delim = ";", escape_double = FALSE, trim_ws = TRUE)
 card$cardio <- factor(card$cardio, labels = c("No", "Yes"))
@@ -28,36 +29,69 @@ ui <- fluidPage(
               inputId="xvar",
               label="Choose independent variable",
               choices=c("smoking","alcohol")
+            ),
+            selectInput(
+              inputId="chart",
+              label="Choose chart type",
+              choices=c("bar","pie")
             )
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("bar")
+           plotOutput("plot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    output$bar <- renderPlot({
-      if(input$xvar=="smoking"){
-        tab<-as.data.frame(table(card$smoke,card$cardio))
-        colnames(tab)<-c("smoking","cardiovascular","count")
-        ggplot(tab,aes(x=smoking,y=count,fill=cardiovascular))+
-        geom_bar(stat="identity")+
-        labs(x="Smoking",y="Count",fill="Cardiovascular Disease")+
-        scale_fill_manual(values=c("No"="grey","Yes"="blue"))+
-        theme_minimal()
+    output$plot <- renderPlot({
+      if(input$chart=="bar"){
+        if(input$xvar=="smoking"){
+          tab<-as.data.frame(table(card$smoke,card$cardio))
+          colnames(tab)<-c("smoking","cardiovascular","count")
+          ggplot(tab,aes(x=smoking,y=count,fill=cardiovascular))+
+          geom_bar(stat="identity")+
+          labs(x="Smoking",y="Count",fill="Cardiovascular Disease")+
+          scale_fill_manual(values=c("No"="grey","Yes"="blue"))+
+          theme_minimal()
+        }
+        else{
+          tab<-as.data.frame(table(card$alco,card$cardio))
+          colnames(tab)<-c("alcohol","cardiovascular","count")
+          ggplot(tab,aes(x=alcohol,y=count,fill=cardiovascular))+
+          geom_bar(stat="identity")+
+          labs(x ="Alcohol",y="Count",fill="Cardiovascular Disease")+
+          scale_fill_manual(values=c("No"="grey","Yes"="blue"))+
+          theme_minimal()
+        }
       }
-      else{
-        tab<-as.data.frame(table(card$alco,card$cardio))
-        colnames(tab)<-c("alcohol","cardiovascular","count")
-        ggplot(tab,aes(x=alcohol,y=count,fill=cardiovascular))+
-        geom_bar(stat="identity")+
-        labs(x ="Alcohol",y="Count",fill="Cardiovascular Disease")+
-        scale_fill_manual(values=c("No"="grey","Yes"="blue"))+
-        theme_minimal()
+      else if(input$chart=="pie"){
+        if(input$xvar=="smoking"){
+          tab<-as.data.frame(table(card$smoke,card$cardio))
+          colnames(tab)<-c("smoking","cardiovascular","count")
+          counts<-tapply(tab$count,tab$smoking,sum)
+          tab$prop<-tab$count/counts[as.character(tab$smoking)]
+          ggplot(tab,aes(x="",y=prop,fill=cardiovascular))+
+            geom_bar(stat="identity",width=1)+
+            coord_polar("y",start=0)+
+            labs(fill="Cardiovascular Disease")+
+            theme_void()+
+            facet_wrap(~smoking)
+        }
+        else{
+          tab<-as.data.frame(table(card$alco,card$cardio))
+          colnames(tab)<-c("alcohol","cardiovascular","count")
+          counts<-tapply(tab$count,tab$alcohol,sum)
+          tab$prop<-tab$count/counts[as.character(tab$alcohol)]
+          ggplot(tab,aes(x="",y=prop,fill=cardiovascular))+
+            geom_bar(stat="identity",width=1)+
+            coord_polar("y",start=0)+
+            labs(fill="Cardiovascular Disease")+
+            theme_void()+
+            facet_wrap(~alcohol)
+        }
       }
     })
 }
